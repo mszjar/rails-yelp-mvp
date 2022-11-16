b695e3429b2f14728304d72945190f201cac5940
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-11-16 15:58:46 +0100

    challenge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(name: 'la famille', phone_number: '320155978', address: 'Lille', category: 'french')
+Restaurant.create!(name: 'mcdo', phone_number: '320155978', address: 'Paris', category: 'chinese')
+Restaurant.create!(name: 'kfc', phone_number: '320155978', address: 'Londre', category: 'italian')
+Restaurant.create!(name: 'subway', phone_number: '320155978', address: 'Tokyo', category: 'japanese')
+Restaurant.create!(name: 'pollo hermanos', phone_number: '320155978', address: 'Wazemmes', category: 'belgian')
